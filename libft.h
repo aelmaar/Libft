d58a601092e58fc510b025f7eb1b057484f68226
Anouar El maaroufi <ael-maar@c2r5p5.1337.ma>
2022-09-28 20:33:49 +0100

    working on some mandatory part functions with the header file

---
+++ libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/28 15:52:11 by ael-maar          #+#    #+#             */
+/*   Updated: 2022/09/28 20:28:04 by ael-maar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+// mandatory part functions
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+
+#endif
