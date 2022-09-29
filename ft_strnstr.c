334a7db8eddc83352fafcf5e76bdb73eca06a1a5
Anouar El maaroufi <ael-maar@c2r5p5.1337.ma>
2022-09-29 16:29:15 +0100

    completed the function ft_strnstr and updating the header file

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,21 +6,38 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 11:52:25 by ael-maar          #+#    #+#             */
-/*   Updated: 2022/09/29 12:57:43 by ael-maar         ###   ########.fr       */
+/*   Updated: 2022/09/29 16:24:06 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
+	size_t	j;
 	size_t	check_str;
+	size_t	needle_len;
 
+	if (needle[0] == '\0')
+		return ((char *)haystack);
 	i = 0;
-	check_str = 0;
+	needle_len = ft_strlen(needle);
 	while (*haystack && i < len)
 	{
-		
+		check_str = 0;
+		j = i;
+		while (haystack[check_str]
+			&& haystack[check_str] == needle[check_str] && j < len)
+		{
+				j++;
+				check_str++;
+		}
+		if (check_str == needle_len)
+			return ((char *)haystack);
+		i++;
+		haystack++;
 	}
+	return (0);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 15:52:11 by ael-maar          #+#    #+#             */
-/*   Updated: 2022/09/29 11:28:20 by ael-maar         ###   ########.fr       */
+/*   Updated: 2022/09/29 15:28:00 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,7 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+size_t	ft_strlen(const char *str);
 
 #endif
