f46146421d0f91433571affb17173a6fba4767ec
Anouar El maaroufi <ael-maar@c2r5p1.1337.ma>
2022-10-03 16:10:29 +0100

    done strtrim and substr functions

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,30 +6,51 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 19:26:57 by ael-maar          #+#    #+#             */
-/*   Updated: 2022/10/02 20:44:35 by ael-maar         ###   ########.fr       */
+/*   Updated: 2022/10/03 14:52:11 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	/*
+	char	*begin_trim;
+	char	*end_trim;
+	char	*trim;
+
+	begin_trim = ft_strnstr(s1, set, ft_strlen(s1));
+	begin_trim = begin_trim + ft_strlen(set);
+	end_trim = ft_strnstr(begin_trim, set, ft_strlen(begin_trim));
+	*end_trim = '\0';
+	trim = ft_strdup(begin_trim);
+	return (trim);
+	*/
+
 	size_t	start;
 	size_t	end;
 	size_t	i;
+	// size_t	temp;
 	char	*str_trim;
 
 	start = 0;
 	i = 0;
 	end = ft_strlen(s1);
-	while (ft_strchr(s1, set[start]))
+
+	while (ft_strchr(set, s1[start]))
 		start++;
-	while (ft_strrchr(s1 + end, set[i]))
-	{
+	while (ft_strchr(set, s1[end]))
 		end--;
-		i++;
-	}
-	str_trim = ft_substr(s1, start, end - start);
+	// str_trim = malloc((end - start + 1) * sizeof(char));
+	// i = 0;
+	// temp = start;
+	// while (i < (end - start))
+	// {
+	// 	str_trim[i++] = s1[temp];
+	// }
+	// str_trim[i] = '\0';
+	str_trim = ft_substr(s1, start, (end - start + 1));
 	return (str_trim);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 19:17:51 by ael-maar          #+#    #+#             */
-/*   Updated: 2022/10/02 20:40:50 by ael-maar         ###   ########.fr       */
+/*   Updated: 2022/10/03 15:38:46 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,25 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub_str;
+	size_t	len_s;
 	size_t	i;
 
-	sub_str = malloc((len + 1) * sizeof(char));
+	len_s = ft_strlen(s);
+	if (ft_strlen(s) > len)
+		sub_str = malloc((len + 1) * sizeof(char));
+	else
+		sub_str = malloc((ft_strlen(s) + 1) * sizeof(char));
 	if (!sub_str)
 		return (0);
 	i = 0;
-	while (s[start] && i < len)
+	if (len_s >= start)
 	{
-		sub_str[i] = s[start];
-		i++;
-		start++;
+		while (s[start] && i < len)
+		{
+			sub_str[i] = s[start];
+			i++;
+			start++;
+		}
 	}
 	sub_str[i] = '\0';
 	return (sub_str);
