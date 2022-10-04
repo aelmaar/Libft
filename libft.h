8d79958944bb696ab8a97d4ebaaac11a34db73d3
Anouar El maaroufi <ael-maar@c2r5p3.1337.ma>
2022-10-04 15:28:27 +0100

    done striteri function and updated the header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 15:52:11 by ael-maar          #+#    #+#             */
-/*   Updated: 2022/10/04 13:27:39 by ael-maar         ###   ########.fr       */
+/*   Updated: 2022/10/04 15:05:48 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,6 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 #endif
