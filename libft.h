e4644cddce79167551c7478ed8326f5818e4fa54
Anouar El maaroufi <ael-maar@c2r5p3.1337.ma>
2022-10-04 13:03:34 +0100

    done itoa function and updated the header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 15:52:11 by ael-maar          #+#    #+#             */
-/*   Updated: 2022/10/03 17:24:07 by ael-maar         ###   ########.fr       */
+/*   Updated: 2022/10/04 12:00:48 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
