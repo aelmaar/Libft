729b5654f8b1d298f6e6fc3d9bc84fe2c06cfb5d
Anouar El maaroufi <ael-maar@c2r5p9.1337.ma>
2022-10-07 13:09:01 +0100

    my memcpy function does not work with a case if both params dst and src are null, so it is fixed

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 18:03:35 by ael-maar          #+#    #+#             */
-/*   Updated: 2022/09/30 19:57:30 by ael-maar         ###   ########.fr       */
+/*   Updated: 2022/10/07 12:48:46 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned char	*src_cast;
 	size_t			i;
 
+	if (!dst && !src)
+		return (0);
 	dst_cast = dst;
 	src_cast = (unsigned char *)src;
 	i = 0;
