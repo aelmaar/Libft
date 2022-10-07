a1f759499025f2b2857dfa8e464a333cb95d0f93
Anouar El maaroufi <ael-maar@c2r5p9.1337.ma>
2022-10-07 15:30:50 +0100

    my lstlast bonus function does not work with a case if the list i sent is null, it is fixed

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 19:13:07 by ael-maar          #+#    #+#             */
-/*   Updated: 2022/10/04 19:17:25 by ael-maar         ###   ########.fr       */
+/*   Updated: 2022/10/07 15:26:05 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
+	if (!lst)
+		return (0);
 	while (lst->next != 0)
 		lst = lst->next;
 	return (lst);
