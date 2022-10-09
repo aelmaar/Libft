87588f8551bc1781752e614d69d25cb405eb58cb
Anouar El maaroufi <ael-maar@c2r5p9.1337.ma>
2022-10-09 11:29:45 +0100

    made some changes to the function in order to work properly

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 19:26:57 by ael-maar          #+#    #+#             */
-/*   Updated: 2022/10/08 19:54:22 by ael-maar         ###   ########.fr       */
+/*   Updated: 2022/10/09 10:41:02 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (!s1)
 		return (0);
 	start = 0;
-	end = ft_strlen(s1);
-	while (ft_strchr(set, s1[start]))
+	end = ft_strlen(s1) - 1;
+	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
-	while (ft_strchr(set, s1[end]))
+	while (s1[end] && end > start && ft_strchr(set, s1[end]))
 		end--;
 	str_trim = ft_substr(s1, start, (end - start + 1));
 	return (str_trim);
